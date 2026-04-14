b12e1015b91b0552339d35f330a9021c996947a0
christel <cwagner@addu.edu.ph>
2026-04-14 15:35:39 +0800

    Enhance type safety in query function by adding generic type parameter

--- basicnextapp/lib/db.ts
+++ basicnextapp/lib/db.ts
@@ -5,7 +5,7 @@ const pool = new Pool({
   ssl: { rejectUnauthorized: false },
 });
 
-export async function query(text: string, params?: any[]) {
-  const res = await pool.query(text, params);
+export async function query<T = any>(text: string, params?: any[]) {
+  const res = await pool.query<T>(text, params);
   return res;
 }
